dd70afedb2929c7a9039333d709383c6c67856b0
KerimCaglar <kerim.caglarr@gmail.com>
2017-01-18 08:35:54 +0300

    To-do-list ilk part

--- To-Do List App/FirstViewController.swift
+++ To-Do List App/FirstViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tasks:[String] = []
 
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let taskObj = UserDefaults.standard.object(forKey: "tasks")
+        
+        
+        if let tasksArr = taskObj as? [String]
+        {
+            tasks = tasksArr
+        }
+        
+        tblView.reloadData()
     }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return tasks.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+        
 
-
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
--- To-Do List App/SecondViewController.swift
+++ To-Do List App/SecondViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var textFieldArea: UITextField!
+    
+    @IBAction func save(_ sender: Any) {
+        
+        let taskObj = UserDefaults.standard.object(forKey: "tasks")
+        
+        var tasks:[String]
+        
+        if let tasksArr = taskObj as? [String]
+        {
+            tasks = tasksArr
+            tasks.append(textFieldArea.text!)
+            
+            print(tasks)
+        }
+        
+        else
+        {
+             tasks = [textFieldArea.text!]
+        }
+        
+        UserDefaults.standard.set(tasks, forKey: "tasks")
+        
+        textFieldArea.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
